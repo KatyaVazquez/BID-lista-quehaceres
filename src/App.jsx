d30818aae88548ace79c21eb1816e2c9f32992d1
Katya Vazquez <katyarocio34@gmail.com>
2023-01-29 19:00:53 -0300

    Lista De Tareas con React!

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Listadequehaceres from './Components/Listadequehaceres';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello! What do you plan to do today?</h1>
+      <Listadequehaceres/>
+    </div>
+  );
+}
+
+export default App;
